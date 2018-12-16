b2a159a04e9d6b6db92c64b746fd55c465ffd59b
Tyler D <3713763+tylerd81@users.noreply.github.com>
2018-12-15 19:09:58 -0500

    day05 part 1 done

---
+++ day05/bin/reaction.dart
@@ -0,0 +1,91 @@
+import 'dart:async';
+import 'dart:io';
+
+void main() {
+  print('Advent Of Code - Day 05');
+
+  String testInput = "dabAcCaCBAcCcaDA";
+  // String input = testInput; // replace with data from file
+
+  var file = new File('input.txt');
+  var input = file.readAsStringSync().trim();
+  // var input = "VvbBfpPFrRyRrYNpYyPDlLdVvNnMmnOCcosOoSoOfkKKkFJ";
+  //var input = "fkKKkFJ";
+  // var input = "dabAcCaCBAcCcaDA";
+  // var input = "CCCAaAaAaAaAaAaAaAaAaAaAaAabB";
+  reactions(input);
+}
+
+// work through the reactions using a list that keeps track of the previous letter
+// each time a reaction happens remove the letter from the previous letter
+// or when there is no reaction add the letter to the list
+int reactions(String input) {
+  // add() and removeLast()
+  var previousChars = [];
+  bool done = false;
+  int nextIndex = 0;
+  String nextChar = null;
+
+  while (!done) {
+    if (nextIndex < input.length) {
+      nextChar = input[nextIndex++];
+
+      // compre previous character and the next char
+      if (previousChars.length != 0 &&
+          isSameChar(previousChars.last, nextChar)) {
+        var prev = previousChars.last;
+        bool reaction = false;
+
+        if (isUpperCase(prev) && isLowerCase(nextChar)) {
+          // if prev is uppercase, next must be lowercase for their
+          // to be a reaction
+          reaction = true;
+        } else if (isLowerCase(prev) && isUpperCase(nextChar)) {
+          // if prev is lower case, next must be uppercase
+          reaction = true;
+        } else {
+          // same character but both are upper or lower case
+          previousChars.add(nextChar);
+        }
+
+        if (reaction) {
+          //previous and next are gone
+          previousChars.removeLast();
+        }
+      } else {
+        previousChars.add(nextChar);
+      }
+    } else {
+      done = true;
+    }
+  }
+
+  // print(previousChars.join(""));
+  print('There are ${previousChars.length} units left');
+  return previousChars.length;
+}
+
+// case insensitive character comparison
+bool isSameChar(String a, String b) {
+  if (a.toLowerCase() == b.toLowerCase()) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+bool isUpperCase(String ch) {
+  if (ch == ch.toUpperCase()) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+bool isLowerCase(String ch) {
+  if (ch == ch.toLowerCase()) {
+    return true;
+  } else {
+    return false;
+  }
+}
